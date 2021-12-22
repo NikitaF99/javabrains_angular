0cedc9ec35cbbf2dd29f4b4d5a8e63473b351fc4
NikitaF99 <nikkiqwerty979@gmail.com>
2021-12-22 11:27:42 +0530

    Using pipes

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -11,8 +11,9 @@ export class BlogListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    //have the initializing under noOnInit so it would be helpful for t4esting
     this.blogPosts.push(new BlogPost('Blog post 1', 'Summary 1'))
-    this.blogPosts.push( new BlogPost('Blog post 2', 'Summary 2'));
+    this.blogPosts.push( new BlogPost('Blog post 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'));
   }
 
 }
---
+++ src/app/truncate.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate'
+})
+export class TruncatePipe implements PipeTransform {
+
+  transform(value:any, args?: any): unknown {
+    //since we are passing argument as 30, it shows 30 chracters
+    const limit = args.length > 0 ? parseInt(args[0],10):20; //by default it truncates to 20 characters
+    const trail = args.length >1 ? args[1]: '...';
+    return value.length > limit ? value.substring(0,limit) + trail : value;
+
+    //limit => how long the test needs to be truncated
+    //trail=>what needs to be put at the end ti indicate there is more text
+  }
+
+}
